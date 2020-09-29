80ed6841986deeabd3353859c0121262c0185893
Nikola Majcen <nikola.majcen@infinum.hr>
2020-09-29 15:54:47 +0200

    Removed usage of @objc in toolbox extension

--- ToolBox/Classes/Core/Internal/ToolBoxInternal.swift
+++ ToolBox/Classes/Core/Internal/ToolBoxInternal.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-@objc
 extension ToolBox {
     
     /// Presents the toolbox with tools from provided view controller.
@@ -15,7 +14,6 @@ extension ToolBox {
     /// - Parameters:
     ///     - tools: Tools which will be available in the toolbox.
     ///     - viewController: The view controller from where will the toolbox be presented.
-    @objc(presentTools:onViewController:)
     func present(tools: [Tool], on viewController: UIViewController) {
         let items = tools.map { NavigationToolTableItem(title: $0.name, navigate: $0.presentPreview(from:)) }
         let section = ToolTableSection(title: "Tools", items: items)
