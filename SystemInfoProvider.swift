1f6c3a7795d81e07553872888782605285ff69cf
Nikola Majcen <nikola.majcen@infinum.hr>
2021-02-05 18:57:30 +0100

    Fixed formatting for time in performance tool

--- SystemInfoProvider.swift
+++ SystemInfoProvider.swift
@@ -17,10 +17,19 @@ class SystemInfoProvider {
 private extension SystemInfoProvider {
 
     func secondsToHoursMinutesSeconds (interval: TimeInterval) -> String {
-        let ti = NSInteger(interval)
-        let s = ti % 60
-        let m = (ti / 60) % 60
-        let h = (ti / 3600)
-        return "\(h):\(m):\(s)"
+        let timeInterval = NSInteger(interval)
+        let seconds = timeInterval % 60
+        let minutes = (timeInterval / 60) % 60
+        let hours = (timeInterval / 3600)
+
+        let secondsString = formattedTime(from: seconds)
+        let minutesString = formattedTime(from: minutes)
+        let hoursString = formattedTime(from: hours)
+
+        return "\(hoursString):\(minutesString):\(secondsString)"
+    }
+
+    func formattedTime(from value: Int) -> String {
+        return value < 10 ? "0\(value)" : "\(value)"
     }
 }
