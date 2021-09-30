a6fed05ffa0c3ca6765aa4edc8555f44c4057aa7
Zvonimir Medak <zvonimir.medak@infinum.com>
2021-09-30 12:45:12 +0200

    added IBAction to close on Done

--- Sentinel/Classes/Core/Internal/SentinelTabBarController.swift
+++ Sentinel/Classes/Core/Internal/SentinelTabBarController.swift
@@ -12,6 +12,14 @@ final class SentinelTabBarController: UITabBarController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
+
+    @IBAction func close(_ sender: Any) {
+        if (navigationController?.viewControllers.count ?? 0) > 1 {
+            navigationController?.popViewController(animated: true)
+        } else {
+            dismiss(animated: true)
+        }
+    }
 }
 
 extension SentinelTabBarController {
