d3ed61064f4b1ba8358047221817e3bd582edd22
Zvonimir Medak <zvonimir.medak@infinum.com>
2021-09-30 12:46:28 +0200

    added initialisation logic for TabBarController

--- Sentinel/Classes/Core/Internal/SentinelInternal.swift
+++ Sentinel/Classes/Core/Internal/SentinelInternal.swift
@@ -15,9 +15,47 @@ extension Sentinel {
     ///     - tools: Tools which will be available in the Sentinel.
     ///     - viewController: The view controller from where will the Sentinel be presented.
     func present(tools: [Tool], on viewController: UIViewController) {
-        let items = tools.map { NavigationToolTableItem(title: $0.name, navigate: $0.presentPreview(from:)) }
-        let section = ToolTableSection(title: "Tools", items: items)
-        let toolTable = ToolTable(name: "Sentinel", sections: [section])
-        toolTable.presentPreview(from: viewController, push: false)
+        var tabBarControllers = createTabBarControllers(with: tools, viewController: viewController)
+        let toolsViewController = createToolsController(with: tools, viewController: viewController)
+        let deviceViewController = createDeviceViewController()
+
+        tabBarControllers.insert(deviceViewController, at: 0)
+        tabBarControllers.insert(toolsViewController, at: 2)
+
+        let tabBarController = UIStoryboard.sentinel.instantiateViewController(ofType: SentinelTabBarController.self)
+        tabBarController.setupViewControllers(with: tabBarControllers)
+        tabBarController.title = "Sentinel"
+
+        let navContoller = UINavigationController(rootViewController: tabBarController)
+        viewController.present(navContoller, animated: true)
+    }
+}
+
+private extension Sentinel {
+    func createTabBarControllers(with tools: [Tool], viewController: UIViewController) -> [UIViewController] {
+        return tools.filter { tool in
+            print(tool.name)
+            return tool.name == "Application" ||
+            tool.name == "Permissions" ||
+            tool.name == "Performance"
+        }.map { $0.createViewController(on: viewController) }
+    }
+
+    func createToolsController(with tools: [Tool], viewController: UIViewController) -> UIViewController {
+        let navigationItems = tools.filter { tool in
+            return tool.name != "Application" &&
+            tool.name != "Permissions" &&
+            tool.name != "Performance"
+        }.map { NavigationToolTableItem(title: $0.name, navigate: $0.presentPreview(from:)) }
+
+        let section = ToolTableSection(title: "Tools", items: navigationItems)
+        let toolsViewController = ToolTable(name: "Tools", sections: [section]).createViewController(on: viewController)
+        toolsViewController.tabBarItem = UITabBarItem(title: "Tools", image: nil, tag: 1)
+        return toolsViewController
+    }
+
+    func createDeviceViewController() -> UIViewController {
+        let deviceInfoItem = DeviceInfoTool()
+        return deviceInfoItem.createViewController()
     }
 }
