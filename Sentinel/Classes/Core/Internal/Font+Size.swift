51bc5f35330f00262448b9dffdbc295555f377a4
Zvonimir Medak <zvonimir.medak@infinum.com>
2025-03-17 10:29:43 +0100

    add font style and some title changes

---
+++ Sentinel/Classes/Core/Internal/Font+Size.swift
@@ -0,0 +1,32 @@
+//
+//  Font+Size.swift
+//  Pods
+//
+//  Created by Zvonimir Medak on 17.03.2025..
+//
+
+import Foundation
+import SwiftUI
+
+enum FontSize: CGFloat {
+    case title1 = 24
+    case subtitle1 = 16
+    case body1 = 14
+    case body2 = 13
+    case caption1 = 11
+}
+
+extension Font {
+
+    static let title1Bold = system(fontSize: .title1, weight: .bold)
+    static let subtitle1Bold = system(fontSize: .subtitle1, weight: .bold)
+    static let body1Bold = system(fontSize: .body1, weight: .bold)
+    static let body1Regular = system(fontSize: .body1, weight: .regular)
+    static let body2Regular = system(fontSize: .body2, weight: .regular)
+    static let body2Bold = system(fontSize: .body2, weight: .bold)
+    static let caption1Regular = system(fontSize: .caption1, weight: .regular)
+
+    private static func system(fontSize: FontSize, weight: Font.Weight) -> Font {
+        system(size: fontSize.rawValue).weight(weight)
+    }
+}
